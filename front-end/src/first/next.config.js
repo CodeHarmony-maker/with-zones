9eea66a67ce556097aca1bcb9ef50e2970d964da
nazakatAli <nazakatalikharal786@gmail.com>
2021-02-15 11:34:30 +0500

    first next config changes

--- front-end/src/first/next.config.js
+++ front-end/src/first/next.config.js
@@ -1,33 +1,33 @@
 const { BLOG_URL } = process.env
 
-module.exports = {
-  rewrites() {
-    return [
-      {
-        source: '/dashboard',
-        destination: `${BLOG_URL}/dashboard`,
-      },
-      {
-        source: '/dashboard/:path*',
-        destination: `${BLOG_URL}/dashboard/:path*`,
-      },
-    ]
-  },
-}
 // module.exports = {
-//   basePath: "/first",
-//   rewrites: async () => {
+//   rewrites() {
 //     return [
 //       {
-//         source: "/home",
-//         destination: `http://localhost:3000/home`,
-//         basePath: false,
+//         source: '/dashboard',
+//         destination: `${BLOG_URL}/dashboard`,
 //       },
 //       {
-//         source: "/home/:path*",
-//         destination: `http://localhost:3000/home/:path*`,
-//         basePath: false,
+//         source: '/dashboard/:path*',
+//         destination: `${BLOG_URL}/dashboard/:path*`,
 //       },
-//     ];
+//     ]
 //   },
-// };
+// }
+module.exports = {
+  basePath: "/first",
+  rewrites: async () => {
+    return [
+      {
+        source: "/dashboard",
+        destination: `${BLOG_URL}/dashboard`,
+        basePath: false,
+      },
+      {
+        source: "/dashboard/:path*",
+        destination: `${BLOG_URL}/dashboard/:path*`,
+        basePath: false,
+      },
+    ];
+  },
+};
